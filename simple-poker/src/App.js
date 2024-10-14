312ec41964ea22895717d74d3012211fb7247ee6
VPLEV23 <bladetheblade12@gmail.com>
2024-10-14 14:17:13 +0200

    Auth telegram

--- simple-poker/src/App.js
+++ simple-poker/src/App.js
@@ -1,56 +1,33 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PokerTable from './components/PokerTable';
+import { TelegramProvider, useTelegram } from './TelegramProvider';
 
-function App() {
-  useEffect(() => {
-    // Check if Telegram WebApp is available
-    if (window.Telegram && window.Telegram.WebApp) {
-      const tg = window.Telegram.WebApp;
+function AppContent() {
+  const { user, webApp } = useTelegram();
 
-      tg.expand(); // Expands the web app to full screen
-
-      // Extract user data from the WebApp
-      const initData = tg.initData; // Authenticated data
-      const initDataUnsafe = tg.initDataUnsafe; // User data
-
-      // Show Telegram's built-in debug popup
-      tg.showDebug();
-
-      console.log('Telegram Init Data:', initData);
-      console.log('Telegram User Data:', initDataUnsafe);
-
-      // Send user data to the backend
-      fetch('http://localhost:3001/api/verifyUser', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          initData,  // Authenticated Telegram data
-          initDataUnsafe,  // Telegram user data
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.success) {
-            console.log('User verified and added to the database:', data);
-          } else {
-            console.error('Error verifying user:', data.message);
-          }
-        })
-        .catch((error) => {
-          console.error('Error sending data to the backend:', error);
-        });
-    } else {
-      // If Telegram WebApp is not available, log a message
-      console.error('Telegram WebApp is not available.');
-    }
-  }, []);
+  // Debug: Log Telegram WebApp data and user data
+  console.log('User:', user);
+  console.log('WebApp:', webApp);
 
   return (
     <div className="App">
-      <PokerTable />
+      {user ? (
+        <div>
+          <h1>Welcome {user.username}</h1>
+          <PokerTable />
+        </div>
+      ) : (
+        <div>Make sure this web app is opened in Telegram client</div>
+      )}
     </div>
+  );
+}
+
+function App() {
+  return (
+    <TelegramProvider>
+      <AppContent />
+    </TelegramProvider>
   );
 }
 
