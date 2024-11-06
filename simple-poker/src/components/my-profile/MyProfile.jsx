67a9fffc767c1e590271ff9c66fe68f8811878ce
dmiroshnichenko02 <danil.miroshnichenko2022@gmail.com>
2024-11-06 15:59:07 +0200

    score

--- simple-poker/src/components/my-profile/MyProfile.jsx
+++ simple-poker/src/components/my-profile/MyProfile.jsx
@@ -16,7 +16,7 @@ const MyProfile = () => {
 			<div className='center'>
 				<div className='balance-profile' style={{ color: '#fff' }}>
 					<img src='/assets/pc1.png' alt='balance' />
-					<span>{balance}</span>
+					<span>{balance - 100}</span>
 				</div>
 				<h2>Your Bank</h2>
 			</div>
